477a1ebf989165192f22761279c5e61deda1a9e5
Zafeerah15 <zafeerah15@gmail.com>
2022-07-27 22:31:21 +0800

    final-zafeerah

--- app/src/main/java/sG/EDU/NP/MAD/friendsOnly/bottomNav/ChatFragment.java
+++ app/src/main/java/sG/EDU/NP/MAD/friendsOnly/bottomNav/ChatFragment.java
@@ -8,7 +8,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+//
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
